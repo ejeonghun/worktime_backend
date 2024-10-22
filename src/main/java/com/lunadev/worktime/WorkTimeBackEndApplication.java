26ffaf64c4d16505d482034d6bc2efc9bd1547bc
ejeonghun <wjdgns4019@gmail.com>
2024-10-22 18:42:59 +0900

    init

---
+++ src/main/java/com/lunadev/worktime/WorkTimeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.lunadev.worktime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTimeBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkTimeBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lunadev/worktime/WorkTimeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunadev.worktime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTimeBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
